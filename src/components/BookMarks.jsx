4e4844117ccfbdbb2acfc0fadeb5dc5a82f5a974
anton <antonismhliotis@gmail.com>
2022-11-30 19:02:20 +0200

    Fixed alignment of bookmarks

--- src/components/BookMarks.jsx
+++ src/components/BookMarks.jsx
@@ -32,53 +32,162 @@ export default function BookMarks({
   };
 
   return (
-    <Stack
-      margin="auto"
-      marginTop={5}
-      direction="row"
-      justifyContent={"space-evenly"}
-      alignItems="baseline"
-    >
-      {accrdArray.map((bookm, i) => (
-        <Accordion key={bookm.id} sx={{ bgcolor: "whitesmoke" }}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography>{bookm.title}</Typography>
-          </AccordionSummary>
-          <Divider>Details</Divider>
-          <AccordionDetails>
-            <Typography variant="p">
-              <NotesIcon
-                color="secondary"
-                sx={{ marginRight: 1, marginBottom: -1 }}
-              />
-              {bookm.content}
-            </Typography>
-            <Divider />
-            <Typography variant="p">
-              <InfoIcon
-                color="info"
-                sx={{ marginRight: 1, marginBottom: -1 }}
-              />
-              {bookm.type}
-            </Typography>
-          </AccordionDetails>
-          <Tooltip title="Edit Bookmark">
-            <IconButton onClick={() => handleEdit(i)}>
-              <EditIcon fontSize="large" />
-            </IconButton>
-          </Tooltip>
+    <Stack direction="row" marginLeft={3} marginTop={4} marginRight={3}>
+      {" "}
+      <Stack
+        direction="column"
+        justifyContent="space-evenly"
+        alignItems="flex-start"
+        spacing={2}
+      >
+        {accrdArray.map((bookm, i) => {
+          return bookm.type === "Work" ? (
+            <Accordion key={bookm.id} sx={{ bgcolor: "whitesmoke" }}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography>{bookm.title}</Typography>
+              </AccordionSummary>
+              <Divider>Details</Divider>
+              <AccordionDetails>
+                <Typography variant="p">
+                  <NotesIcon
+                    color="secondary"
+                    sx={{ marginRight: 1, marginBottom: -1 }}
+                  />
+                  {bookm.content}
+                </Typography>
+                <Divider />
+                <Typography variant="p">
+                  <InfoIcon
+                    color="info"
+                    sx={{ marginRight: 1, marginBottom: -1 }}
+                  />
+                  {bookm.type}
+                </Typography>
+              </AccordionDetails>
+              <Tooltip title="Edit Bookmark">
+                <IconButton onClick={() => handleEdit(i)}>
+                  <EditIcon fontSize="large" />
+                </IconButton>
+              </Tooltip>
 
-          <Tooltip title="Delete Bookmark">
-            <IconButton onClick={() => handleDelete(i)}>
-              <DeleteIcon fontSize="large" />
-            </IconButton>
-          </Tooltip>
-        </Accordion>
-      ))}
+              <Tooltip title="Delete Bookmark">
+                <IconButton onClick={() => handleDelete(i)}>
+                  <DeleteIcon fontSize="large" />
+                </IconButton>
+              </Tooltip>
+            </Accordion>
+          ) : (
+            console.log("Hello")
+          );
+        })}
+      </Stack>
+      <Stack
+        margin="auto"
+        direction="column"
+        justifyContent="space-evenly"
+        alignItems="center"
+        spacing={2}
+      >
+        {accrdArray.map((bookm, i) => {
+          return bookm.type === "Leisure" ? (
+            <Accordion key={bookm.id} sx={{ bgcolor: "whitesmoke" }}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography>{bookm.title}</Typography>
+              </AccordionSummary>
+              <Divider>Details</Divider>
+              <AccordionDetails>
+                <Typography variant="p">
+                  <NotesIcon
+                    color="secondary"
+                    sx={{ marginRight: 1, marginBottom: -1 }}
+                  />
+                  {bookm.content}
+                </Typography>
+                <Divider />
+                <Typography variant="p">
+                  <InfoIcon
+                    color="info"
+                    sx={{ marginRight: 1, marginBottom: -1 }}
+                  />
+                  {bookm.type}
+                </Typography>
+              </AccordionDetails>
+              <Tooltip title="Edit Bookmark">
+                <IconButton onClick={() => handleEdit(i)}>
+                  <EditIcon fontSize="large" />
+                </IconButton>
+              </Tooltip>
+
+              <Tooltip title="Delete Bookmark">
+                <IconButton onClick={() => handleDelete(i)}>
+                  <DeleteIcon fontSize="large" />
+                </IconButton>
+              </Tooltip>
+            </Accordion>
+          ) : (
+            console.log("Hello")
+          );
+        })}
+      </Stack>
+      <Stack
+        direction="column"
+        justifyContent="space-evenly"
+        alignItems="flex-end"
+        spacing={2}
+      >
+        {accrdArray.map((bookm, i) => {
+          return bookm.type === "Personal" ? (
+            <Accordion key={bookm.id} sx={{ bgcolor: "whitesmoke" }}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography>{bookm.title}</Typography>
+              </AccordionSummary>
+              <Divider>Details</Divider>
+              <AccordionDetails>
+                <Typography variant="p">
+                  <NotesIcon
+                    color="secondary"
+                    sx={{ marginRight: 1, marginBottom: -1 }}
+                  />
+                  {bookm.content}
+                </Typography>
+                <Divider />
+                <Typography variant="p">
+                  <InfoIcon
+                    color="info"
+                    sx={{ marginRight: 1, marginBottom: -1 }}
+                  />
+                  {bookm.type}
+                </Typography>
+              </AccordionDetails>
+              <Tooltip title="Edit Bookmark">
+                <IconButton onClick={() => handleEdit(i)}>
+                  <EditIcon fontSize="large" />
+                </IconButton>
+              </Tooltip>
+
+              <Tooltip title="Delete Bookmark">
+                <IconButton onClick={() => handleDelete(i)}>
+                  <DeleteIcon fontSize="large" />
+                </IconButton>
+              </Tooltip>
+            </Accordion>
+          ) : (
+            console.log("Hello")
+          );
+        })}
+      </Stack>
     </Stack>
   );
 }
