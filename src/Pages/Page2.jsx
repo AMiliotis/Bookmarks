0bb8859f7cd7e1f5236ef9898fee6046d68f1447
anton <antonismhliotis@gmail.com>
2022-12-11 23:58:47 +0200

    Added localStorage and delete LocalStorage

--- src/Pages/Page2.jsx
+++ src/Pages/Page2.jsx
@@ -1,8 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Counter from "../components/Counter";
 
 export default function Page2() {
   const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const data = window.localStorage.getItem("MY_COUNTER");
+    console.log(JSON.parse(data));
+    if (data !== null) setCounter(JSON.parse(data));
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem("MY_COUNTER", JSON.stringify(counter));
+  }, [counter]);
 
   return (
     <>
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -12,7 +12,9 @@ export default function Counter({ counter, setCounter }) {
         </Typography>
       </AppBar>
       <Box
-        sx={{ background: "linear-gradient(red,blue,yellow)" }}
+        sx={{
+          background: "gray",
+        }}
         marginTop={2}
         display="flex"
         justifyContent="center"
@@ -21,6 +23,29 @@ export default function Counter({ counter, setCounter }) {
         <Typography variant="h1" fontSize={120}>
           {counter}
         </Typography>
+      </Box>
+      <Typography variant="p">
+        The counter State gets saved on{" "}
+        <Typography fontWeight="bold" display="inline" sx={{ color: "blue" }}>
+          Local Storage
+        </Typography>
+        , if you want to
+        <Typography fontWeight="bold" display="inline" sx={{ color: "red" }}>
+          &nbsp; delete
+        </Typography>{" "}
+        the saved state click the button bellow
+      </Typography>
+      <Box marginTop={1}>
+        <Button
+          sx={{ bgcolor: "orange", color: "black" }}
+          onClick={() => {
+            localStorage.clear();
+            window.location.reload(false);
+          }}
+          variant="contained"
+        >
+          Delete Local Storage
+        </Button>
       </Box>
       <Box
         display="flex"
@@ -31,13 +56,15 @@ export default function Counter({ counter, setCounter }) {
         {" "}
         <ButtonGroup size="large" variant="text" aria-label="text button group">
           <Button
-            variant="outlined"
+            variant="contained"
+            sx={{ bgcolor: "red" }}
             onClick={() => setCounter((prevCount) => prevCount - 1)}
           >
             Minus
           </Button>
           <Button
-            variant="outlined"
+            sx={{ bgcolor: "green" }}
+            variant="contained"
             onClick={() => setCounter((prevCount) => prevCount + 1)}
           >
             Plus
